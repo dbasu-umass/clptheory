3c4783a6a31f86425e566c0157fc3cdb3be8cfc2
Deepankar Basu <dbasu@umass.edu>
2023-03-24 08:08:30 -0400

    Incorporated conditions for PF theorem

--- R/ppstdint1.R
+++ R/ppstdint1.R
@@ -63,61 +63,71 @@ ppstdint1 <- function(A, l, b, Q, l_simple){
   nn_M <- ifelse(min(M)>=0,1,0)
   # Is M irreducible?
   ir_M <- ifelse(popdemo::isIrreducible(M),1,0)
-
-  # ---- Uniform rate of profit
-  maxEigenv <- max(Mod(eigen(M)$values))
-  r <- (1/maxEigenv)-1
-
-  # -- Maximal rate of profit (when b is the 0 vector)
-  R <- 1/(max(Mod(eigen(A)$values)))-1
-
-  # ----- Solve for price of production vector
-  # Rel Price = First column of eigen vector matrix of M
-  # The vector has all real elements (of the same sign)
-  # If any element <0 then all elements <0; Hence, multiply with -1
-  p_rel_neg <- (-1)*Re(eigen(M)$vectors[,1])
-  p_rel_pos <- Re(eigen(M)$vectors[,1])
-  if (Re(eigen(M)$vectors[1,1])<0) {
-    p_rel <- p_rel_neg
-  }else{
-    p_rel <- p_rel_pos
+  
+  # Perron-Frobenius theorem applies only if nn_M==1 and ir_M==1
+  if(nn_M==0){
+    stop("M is not nonnegative. Perron-Frobenius Theorem will not apply.")
+  } else if(ir_M==0){
+    stop("M is not irreducible. Perron-Frobenius Theorem will not apply.")
+  } else{
+    
+    # ---- Uniform rate of profit
+    maxEigenv <- max(Mod(eigen(M)$values))
+    r <- (1/maxEigenv)-1
+    
+    # -- Maximal rate of profit (when b is the 0 vector)
+    R <- 1/(max(Mod(eigen(A)$values)))-1
+    
+    # ----- Solve for price of production vector
+    # Rel Price = First column of eigen vector matrix of M
+    # The vector has all real elements (of the same sign)
+    # If any element <0 then all elements <0; Hence, multiply with -1
+    p_rel_neg <- (-1)*Re(eigen(M)$vectors[,1])
+    p_rel_pos <- Re(eigen(M)$vectors[,1])
+    if (Re(eigen(M)$vectors[1,1])<0) {
+      p_rel <- p_rel_neg
+    }else{
+      p_rel <- p_rel_pos
+    }
+    
+    # ---- Vector of values
+    # Note: we use the labor input adjusted for complexity
+    I <- diag(ncol(A))
+    lambda <- l_simple%*%solve(I - A)
+    colnames(lambda) <- colnames(l_simple)
+    
+    # Normalization 1 using gross output
+    mev_num_1 <- sum(matrix(Q,ncol=1))
+    mev_den_1 <- (matrix(lambda,nrow=1)%*%matrix(Q,ncol=1))
+    mev_1 <- mev_num_1/mev_den_1
+    
+    # Normalization 2 using gross output
+    mev_num_2 <- sum(matrix(Q,ncol=1))
+    mev_den_2 <- (matrix(p_rel,nrow=1)%*%matrix(Q,ncol=1))
+    mev_2 <- mev_num_2/mev_den_2
+    
+    # ----- Absolute price of production vector
+    p_abs <- mev_2[1,1]*matrix(p_rel,nrow=1)
+    colnames(p_abs) <- colnames(l)
+    
+    # Direct prices
+    direct_p <- mev_1[1,1]*matrix(lambda, nrow = 1)
+    colnames(direct_p) <- colnames(l_simple)
+    
+    # ----- Results as a list
+    return(list(meig = maxEigenv,
+                urop = r,
+                mrop = R,
+                ppabs = p_abs,
+                pprel = p_rel,
+                lvalues = lambda,
+                dprice = direct_p,
+                mevg = mev_1[1,1],
+                mnonneg = nn_M,
+                mirred = ir_M
+              )
+           )
   }
 
-  # ---- Vector of values
-  # Note: we use the labor input adjusted for complexity
-  I <- diag(ncol(A))
-  lambda <- l_simple%*%solve(I - A)
-  colnames(lambda) <- colnames(l_simple)
-
-  # Normalization 1 using gross output
-  mev_num_1 <- sum(matrix(Q,ncol=1))
-  mev_den_1 <- (matrix(lambda,nrow=1)%*%matrix(Q,ncol=1))
-  mev_1 <- mev_num_1/mev_den_1
-
-  # Normalization 2 using gross output
-  mev_num_2 <- sum(matrix(Q,ncol=1))
-  mev_den_2 <- (matrix(p_rel,nrow=1)%*%matrix(Q,ncol=1))
-  mev_2 <- mev_num_2/mev_den_2
-
-  # ----- Absolute price of production vector
-  p_abs <- mev_2[1,1]*matrix(p_rel,nrow=1)
-  colnames(p_abs) <- colnames(l)
-
-  # Direct prices
-  direct_p <- mev_1[1,1]*matrix(lambda, nrow = 1)
-  colnames(direct_p) <- colnames(l_simple)
-
-  # ----- Results as a list
-  return(list(meig = maxEigenv,
-              urop = r,
-              mrop = R,
-              ppabs = p_abs,
-              pprel = p_rel,
-              lvalues = lambda,
-              dprice = direct_p,
-              mevg = mev_1[1,1],
-              mnonneg = nn_M,
-              mirred = ir_M
-            )
-      )
+  
 }
