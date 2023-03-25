b5c4a0c2baabb9cf5df4d52e100f0e9e83a4f4a3
Deepankar Basu <dbasu@umass.edu>
2023-03-25 10:40:57 -0400

    Added nobs and expanded description of funciton

--- R/regtestrel.R
+++ R/regtestrel.R
@@ -1,6 +1,6 @@
 #' Regression-based Measures of Deviation.
 #'
-#' This function computes various regression based measures of deviation between the vector of all possible relative labor values and the vector of all possible relative prices of production.
+#' This function computes various regression based measures of deviation between the vector of all possible relative labor values and the vector of all possible relative prices of production. It runs a log-log and a level-level regression of relative prices on relative values and test the joint null hypothesis that the intercept is 0 and the slope is 1. 
 #'
 #' @param x price vector (1 x n).
 #' @param y value vector (1 x n).
@@ -15,11 +15,13 @@
 #' \item{r2lg}{R-squared in the log-log regression}
 #' \item{fstatlg}{F-stat of the null hypothesis that a0=0 and a1=1 in the log-log regression}
 #' \item{pvallg}{P-value of the null hypothesis that a0=0 and a1=1 in the log-log regression}
+#' \item{nlg}{Number of observations in the log-log regression}
 #' \item{a0lv}{Intercept in the level-level regression}
 #' \item{a1lv}{Slope in the level-level regression}
 #' \item{r2lv}{R-squared in the level-level regression}
 #' \item{fstatlv}{F-stat of the null hypothesis that a0=0 and a1=1 in the level-level regression}
 #' \item{pvallv}{P-value of the null hypothesis that a0=0 and a1=1 in the level-level regression}
+#' \item{nlv}{Number of observations in the level-level regression}
 #' 
 #' 
 #'
@@ -95,11 +97,13 @@ regtestrel <- function(x,y){
       r2lg = summary(dev1)$r.squared,
       fstatlg = l1$F[2],
       pvallg = round(l1$`Pr(>F)`[2], digits=4),
+      nlg = nobs(dev1),
       a0lv = dev2$coefficients[1],
       a1lv = dev2$coefficients[2],
       r2lv = summary(dev2)$r.squared,
       fstatlv = l2$F[2],
-      pvallv = round(l2$`Pr(>F)`[2], digits=4)
+      pvallv = round(l2$`Pr(>F)`[2], digits=4),
+      nlv = nobs(dev2)
     )
   )
 }
