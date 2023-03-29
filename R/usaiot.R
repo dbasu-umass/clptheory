afb5dbce0d5fe5e94839d41dcb3b726997a9c9d6
Deepankar Basu <dbasu@umass.edu>
2023-03-29 09:16:50 -0400

    Documentation for IO Tables

---
+++ R/usaiot.R
@@ -0,0 +1,9 @@
+#' USA IO Table
+#'
+#' Input Output Tables for the US economy from the World Input Output Database.
+#'
+#' @source <https://doi.org/10.34894/PJ2M1C>
+#' @format Input Output table for USA for 15 years, 2000-2014.
+#' @examples
+#' data(usaiot)
+"usaiot"
