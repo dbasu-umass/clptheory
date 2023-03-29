9153547ecfb29464673f97766642648346fc4490
Deepankar Basu <dbasu@umass.edu>
2023-03-29 09:16:37 -0400

    Documentation for RWB

---
+++ R/usarwb.R
@@ -0,0 +1,9 @@
+#' Real Wage Bundle, USA
+#'
+#' Personal Consumption Expenditure from the Input Output Table for the USA. This data is used to construct the real wage bundle for computing the price of production vector.
+#'
+#' @source <https://doi.org/10.34894/PJ2M1C>
+#' @format Consumption expenditure on the output of 53 industries for USA for 15 years, 2000-2014.
+#' @examples
+#' data(usarwb)
+"usarwb"
