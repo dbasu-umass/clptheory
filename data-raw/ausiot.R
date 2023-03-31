21fe369104a0aae1172ed94722951c5e1bba4e74
Deepankar Basu <dbasu@umass.edu>
2023-03-31 11:31:13 -0400

    Create AUS io table

---
+++ data-raw/ausiot.R
@@ -0,0 +1,17 @@
+# libraries
+library(tidyverse)
+library(readxl)
+library(readr)
+
+# --- IO Data for input-output matrix
+# Source: WIOD 2016 Release (https://data.oecd.org/conversion/exchange-rates.htm?fbclid=IwAR2Bn6JlF8WfAbVhwDEiKFhG0_rsTu-iLq1PW47o1ujebwc7CWF0e0wUx_A)
+ausiot <- read_excel(
+  path="~/Dropbox/WIOD/Data/AUS_NIOT_nov16.xlsx",
+  sheet = "National IO-tables"
+) %>% 
+  dplyr::filter(
+    Origin=="Domestic"
+  ) %>%
+  as.data.frame()
+
+usethis::use_data(ausiot, overwrite = TRUE)
