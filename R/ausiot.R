96668cbbb20c26686bb50938926b04fef265fae8
Deepankar Basu <dbasu@umass.edu>
2023-03-31 11:31:43 -0400

    Help page for AUS io table

---
+++ R/ausiot.R
@@ -0,0 +1,9 @@
+#' AUS IO Table
+#'
+#' Input Output Tables for the Australian economy from the World Input Output Database.
+#'
+#' @source <https://doi.org/10.34894/PJ2M1C>
+#' @format Input Output table for Australia for 15 years, 2000-2014.
+#' @examples
+#' ausiot[1:3,1:3]
+"ausiot"
