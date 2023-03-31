2f362983a09f36eb930b125c6e8b5043245658ab
Deepankar Basu <dbasu@umass.edu>
2023-03-31 10:08:47 -0400

    Updated code to create IO data

--- data-raw/usaiot.R
+++ data-raw/usaiot.R
@@ -4,17 +4,13 @@ library(readxl)
 library(readr)
 
 # --- IO Data for input-output matrix
-# Source: WIOD 2016 Release
+# Source: WIOD 2016 Release (https://data.oecd.org/conversion/exchange-rates.htm?fbclid=IwAR2Bn6JlF8WfAbVhwDEiKFhG0_rsTu-iLq1PW47o1ujebwc7CWF0e0wUx_A)
 usaiot <- read_excel(
-  path="~/MyRProjects/clptheory/data-raw/USA_NIOT_nov16.xlsx",
+  path="~/Dropbox/WIOD/Data/AUS_NIOT_nov16.xlsx",
   sheet = "National IO-tables"
-) %>%
+) %>% 
   dplyr::filter(
     Origin=="Domestic"
-  ) %>%
-  dplyr::select(
-    -c("Description","Origin",
-       "CONS_h", "CONS_np","CONS_g","GFCF","INVEN","EXP","GO")
   ) %>%
   as.data.frame()
 
