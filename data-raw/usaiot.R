2da947e9554dbaa297e75545d0623d6434172068
Deepankar Basu <dbasu@umass.edu>
2023-03-31 11:28:47 -0400

    Corrected name of IO file being read in

--- data-raw/usaiot.R
+++ data-raw/usaiot.R
@@ -6,7 +6,7 @@ library(readr)
 # --- IO Data for input-output matrix
 # Source: WIOD 2016 Release (https://data.oecd.org/conversion/exchange-rates.htm?fbclid=IwAR2Bn6JlF8WfAbVhwDEiKFhG0_rsTu-iLq1PW47o1ujebwc7CWF0e0wUx_A)
 usaiot <- read_excel(
-  path="~/Dropbox/WIOD/Data/AUS_NIOT_nov16.xlsx",
+  path="~/Dropbox/WIOD/Data/USA_NIOT_nov16.xlsx",
   sheet = "National IO-tables"
 ) %>% 
   dplyr::filter(
